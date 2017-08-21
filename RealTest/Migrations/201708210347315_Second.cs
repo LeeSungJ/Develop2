a7fd94a915c1b40cff869fd2a4b5c204a3a0bb0f
LeeSungJ <iew10@naver.com>
2017-08-21 14:49:31 +0900

    Conversion 2

--- RealTest/Migrations/201708210347315_Second.cs
+++ RealTest/Migrations/201708210347315_Second.cs
@@ -3,15 +3,16 @@ namespace RealTest.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Initial : DbMigration
+    public partial class Second : DbMigration
     {
         public override void Up()
         {
-            
+            RenameTable(name: "dbo.Movies", newName: "GSModels");
         }
         
         public override void Down()
         {
+            RenameTable(name: "dbo.GSModels", newName: "Movies");
         }
     }
 }
