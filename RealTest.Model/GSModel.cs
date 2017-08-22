93202b8ac2a2446ca0e0761ebdb07b030ebb11e4
LeeSungJ <iew10@naver.com>
2017-08-22 12:14:49 +0900

    Web API

--- RealTest.Model/GSModel.cs
+++ RealTest.Model/GSModel.cs
@@ -15,7 +15,7 @@ namespace RealTest.Model
         [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        //[Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+using RealTest.Model;
+
+namespace WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private MovieDBContext2 db = new MovieDBContext2();
+        DBCon con = new DBCon();
+
+        
+        
+
+        public IEnumerable<GSModel> GetAllProducts()
+        {
+            var movies = from m in db.Movies2
+                         select m;
+
+            return movies.ToList();
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            GSModel movie = db.Movies2.Find(id);
+
+            if(movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+    
+    }
+}
